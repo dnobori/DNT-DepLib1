bd93d9b9879fa7d77f676b43f134fbe77d6d4f3d
Daiyuu Nobori <daiyuu.git@softether.co.jp>
2018-09-16 12:42:06 +0900

    aaa

--- DNT-DepLib1/Class1.cs
+++ DNT-DepLib1/Class1.cs
@@ -8,7 +8,7 @@ namespace Lib1
     {
         public static void Hello()
         {
-            Console.WriteLine("Hello World #2");
+            Console.WriteLine("Hello World #3");
         }
     }
 }
